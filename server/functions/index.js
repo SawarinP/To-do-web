7dc5bd77ce21e0f34e153149152390bc069e4342
SawarinP <Sawarinp@kkumail.com>
2025-06-26 23:58:41 +0700

    add firebase

---
+++ server/functions/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+require('./db');
+
+const taskRoutes = require('./routes/taskRoutes');
+
+const functions = require('firebase-functions');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+
+exports.api = functions.https.onRequest(app);
